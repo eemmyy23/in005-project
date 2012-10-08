83607f4402425540e39eb26b65d5a9fb7459d4cc
emi.puscalau <emi.puscalau@224ec4bb-3926-5ca4-3c0a-f09e3d75ea54>
2012-10-08 17:25:26 +0000

    fixed startup.bash

--- startup.bash
+++ startup.bash
@@ -1,11 +1,13 @@
 #test if length of "STRING" is non-zero.
 if [ -n "$SSH_CLIENT" ];
  then
-  #where is our work folder
-  export PRJ_PATH='/home/emi/Dropbox/EMI-RMA/googleSVN/in005-project'
-
-  #enter into the project folder
-  cd $PRJ_PATH
+  relativeDir=`dirname $BASH_SOURCE`
+  #we will stay in our project root-folder all the time
+  cd $relativeDir
+  
+  #get the absolute path to project folder
+  export PRJ_DIR=`pwd`
+  echo "project dir $PRJ_DIR"
 
   #text that will apear after login
   echo "Robot interface (IN005 PROJECT - RMA - 2012)"
