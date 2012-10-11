8be3ebbca45091a4f2d7bf3d0e6d987ff0604781
emi.puscalau@gmail.com <emi.puscalau@gmail.com@224ec4bb-3926-5ca4-3c0a-f09e3d75ea54>
2012-10-11 14:55:26 +0000

    1

--- trunk/bash/info.sh
+++ trunk/bash/info.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 #whoami
-#source /home/emi/work_electric/setup.bash
+source /home/emi/work_electric/setup.bash
 #export ROS_IP=192.168.1.2
 #export ROS_MASTER_URI="http://192.168.1.2:11311"
 #export ROS_HOME="/home/emi/.ros"
